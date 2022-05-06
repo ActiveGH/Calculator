477c848b52067353ee276abd92b6baf51a8b9773
ActiveGH <86920606+ActiveGH@users.noreply.github.com>
2022-05-06 23:48:54 +0300

    Added more operators
    Added "+" "-" "/" "*"

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 
-num1 = input("Enter a number:")
-num2 = input("Enter another number:")
-result = float(num1) + float(num2)
-print(result)
+num1 = float(input("Enter a number: "))
+operator = input("Enter an operator: ")
+num2 = float(input("Enter another number: "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "/":
+    print(num1 / num2)
+elif operator == "*":
+    print(num1 * num2)
+else:
+    print("Error: Invalid operator")
