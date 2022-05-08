b96120d3bb5e67e2d55e1d9259b33082e34fa883
ActiveGH <86920606+ActiveGH@users.noreply.github.com>
2022-05-08 12:54:17 +0300

    Added powers by useing ^

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
+# Asking user for numbers and operator
 num1 = float(input("Enter a number: "))
 operator = input("Enter an operator: ")
 num2 = float(input("Enter another number: "))
 
+# Operators
 if operator == "+":
     print(num1 + num2)
 elif operator == "-":
@@ -11,5 +13,7 @@ elif operator == "/":
     print(num1 / num2)
 elif operator == "*":
     print(num1 * num2)
+elif operator == "^":
+    print(num1 ** num2)
 else:
     print("Error: Invalid operator")
